cc6e15285fb26e49b354519d077b9faa59aef50a
saeid <emadisaeidabad@gmail.com>
2026-02-11 12:50:26 +0330

    add NotificationCloseAllHeader for close all notifs.

--- Resources.cmake
+++ Resources.cmake
@@ -113,6 +113,7 @@ set(RESOURCES_COMPONENTS
 
     # Notifications
     Qml/View/Components/Notification/NotificationItem.qml
+    Qml/View/Components/Notification/NotificationCloseAllHeader.qml
 )
 
 
