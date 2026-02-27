aaa14c01ce51e88ef984d8c4e9e4a8d733fb0961
Aghaei <avazaghaei@yahoo.com>
2026-02-27 23:24:23 +0330

    updated the backend

--- backend.cmake
+++ backend.cmake
@@ -15,6 +15,7 @@ set(SOURCES_BACKEND
     Src/Git/GitBundle.cpp
     Src/Git/GitConfig.cpp
     Src/Git/GitStash.cpp
+    Src/Git/GitMerge.cpp
 
     Src/Git/Models/Remote.cpp
     Src/Git/Models/Commit.cpp
@@ -47,6 +48,7 @@ set(HEADERS_BACKEND
     Src/Git/GitBundle.h
     Src/Git/GitConfig.h
     Src/Git/GitStash.h
+    Src/Git/GitMerge.h
 
     Src/Git/Models/Remote.h
     Src/Git/Models/Commit.h
