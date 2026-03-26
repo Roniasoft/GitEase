113f362cc74a36b832632ee08daf56ab38755d32
Aghaei <avazaghaei@yahoo.com>
2026-03-26 12:28:15 +0330

    added the mutual conflict popup for both merge and rebase

--- Resources.cmake
+++ Resources.cmake
@@ -152,8 +152,7 @@ set(RESOURCES_POPUPS
     Qml/View/Popups/NotificationCenterPopup.qml
     Qml/View/Popups/ManageStashPopup.qml
     Qml/View/Popups/FetchSummaryPopup.qml
-    Qml/View/Popups/MergeConflictPopup.qml
-    Qml/View/Popups/RebaseConflictPopup.qml
+    Qml/View/Popups/ConflictPopup.qml
 
 )
 
