a6ccb2f52f19959984c353879edbf34b67934dfe
Bardia-Khodadadeh <bardiak1378@gmail.com>
2026-06-15 15:10:29 +0330

    change reset options text

--- Qml/Core/Scripts/CommitGraphMenuBuilder.js
+++ Qml/Core/Scripts/CommitGraphMenuBuilder.js
@@ -120,9 +120,9 @@ function buildMenu(state) {
         icon: "reset",
         action: "reset",
         subItems: [
-            {text: "--Soft (Keep all changes)",  icon: "resetSoft",  action: "resetSoft",  payload: { hash: state.fullHash }},
-            {text: "--Mixed (reset only index)", icon: "resetMixed", action: "resetMixed", payload: { hash: state.fullHash }},
-            {text: "--Hard (discard and reset)", icon: "resetHard",  action: "resetHard",  payload: { hash: state.fullHash }},
+           {text: "--Soft (Keep all changes)",  icon: "resetSoft",  action: "resetSoft",  payload: { hash: state.fullHash }},
+           {text: "--Mixed (reset index to commit)", icon: "resetMixed", action: "resetMixed", payload: { hash: state.fullHash }},
+           {text: "--Hard (discard all changes)", icon: "resetHard",  action: "resetHard",  payload: { hash: state.fullHash }},
         ]
     });
 
