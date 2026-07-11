94e4eab0417f0a8ab6525d1a8549fe1006bd4803
Bardia-Khodadadeh <bardiak1378@gmail.com>
2026-07-11 10:28:52 +0330

    add and use switch component

--- Resources.cmake
+++ Resources.cmake
@@ -176,6 +176,7 @@ set(RESOURCES_COMPONENTS
     Qml/View/Components/Pages/RulesPage/RuleChip.qml
     Qml/View/Components/Pages/RulesPage/OptionRow.qml
     Qml/View/Components/Pages/RulesPage/BasicInfoRect.qml
+    Qml/View/Components/Pages/RulesPage/ModernSwitch.qml
     Qml/View/Components/Pages/RulesPage/CommitMessageSettings.qml
     Qml/View/Components/GraphView/GraphViewHeader.qml
     Qml/View/Components/GraphView/DateField.qml
