b80b14d6981ee527a5d6d2f17674a34e4beabf95
Bardia-Khodadadeh <bardiak1378@gmail.com>
2026-07-11 10:51:20 +0330

    added branch naming settings page

--- Resources.cmake
+++ Resources.cmake
@@ -171,6 +171,7 @@ set(RESOURCES_COMPONENTS
     Qml/View/Components/Pages/RulesPage/BasicInfoRect.qml
     Qml/View/Components/Pages/RulesPage/ModernSwitch.qml
     Qml/View/Components/Pages/RulesPage/CommitMessageSettings.qml
+    Qml/View/Components/Pages/RulesPage/BranchNamingSettings.qml
     Qml/View/Components/GraphView/GraphViewHeader.qml
     Qml/View/Components/GraphView/DateField.qml
     Qml/View/Components/GraphView/ResizableColumnHeader.qml
